f7dcc566ba8f8fb3668798d209bc1c2ed631d8c4
Andrew Marquez <andy2mgcc@gmail.com>
2023-10-07 10:47:20 +0530

    Allow submitting multiple batches at a time

--- app/controllers/sentiment_controller.rb
+++ app/controllers/sentiment_controller.rb
@@ -1,6 +1,13 @@
 class SentimentController < ApplicationController
   def index
     @submitted_text = params&.dig(:sentiment_form, :text)
-    @sentiment = OpenaiHelper.get_sentiment(@submitted_text) if @submitted_text
+    @text_array = @submitted_text&.split("\n\n")
+    @sentiment_array = []
+
+    if @text_array
+      @text_array.each do |text|
+        @sentiment_array << OpenaiHelper.get_sentiment(text)
+      end
+    end
   end
 end
